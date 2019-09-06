ef46471156113e52a94a017ec0bc51772a931741
Anatoliy Korovin <antkorwin@gmail.com>
2019-09-07 00:05:16 +1000

    add a few tests

--- statemachine-utils-core/src/test/java/com/jupitertools/statemachineutils/persist/InMemoryStateMachinePersistTest.java
+++ statemachine-utils-core/src/test/java/com/jupitertools/statemachineutils/persist/InMemoryStateMachinePersistTest.java
@@ -23,4 +23,45 @@ class InMemoryStateMachinePersistTest {
 		StateMachineContext readContext = persist.read(id);
 		assertThat(context).isEqualTo(readContext);
 	}
+
+	@Test
+	void writeMultipleAndReadSingle() throws Exception {
+		// Arrange
+		InMemoryStateMachinePersist persist = new InMemoryStateMachinePersist();
+		UUID firstId = UUID.randomUUID();
+		UUID secondId = UUID.randomUUID();
+		StateMachineContext firstContext = mock(StateMachineContext.class);
+		StateMachineContext secondContext = mock(StateMachineContext.class);
+		// Act
+		persist.write(firstContext, firstId);
+		persist.write(secondContext, secondId);
+		// Assert
+		StateMachineContext first = persist.read(firstId);
+		StateMachineContext second = persist.read(secondId);
+		assertThat(first).isEqualTo(firstContext);
+		assertThat(second).isEqualTo(secondContext);
+	}
+
+	@Test
+	void readNotExisted() throws Exception {
+		// Arrange
+		InMemoryStateMachinePersist persist = new InMemoryStateMachinePersist();
+		// Act
+		StateMachineContext first = persist.read(UUID.randomUUID());
+		// Assert
+		assertThat(first).isNull();
+	}
+
+	@Test
+	void writeAndDelete() throws Exception {
+		// Arrange
+		InMemoryStateMachinePersist persist = new InMemoryStateMachinePersist();
+		UUID id = UUID.randomUUID();
+		StateMachineContext context = mock(StateMachineContext.class);
+		// Act
+		persist.write(context, id);
+		persist.remove(id);
+		// Assert
+		assertThat(persist.read(id)).isNull();
+	}
 }
